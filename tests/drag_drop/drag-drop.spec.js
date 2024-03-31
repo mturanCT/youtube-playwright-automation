27a32522168acd50a37927ed822d51cdb7788242
davidtrn <mturanusa@gmail.com>
2024-03-31 09:36:36 -0500

    tags and page object

--- tests/drag_drop/drag-drop.spec.js
+++ tests/drag_drop/drag-drop.spec.js
@@ -10,10 +10,15 @@ test("Drag & Drop Practice",{tag:["@hlr", "@smoke", "@regression"]},  async ({pa
     await expect(blueBox).toHaveText("Drag the small circle here ...")
     await page.locator("#draggable").dragTo(blueBox); 
     await expect(blueBox).toHaveText("You did great!")
+    await expect(blueBox).toHaveCSS('background-color',"rgb(63, 81, 181)" );
+
+    /*
+    javascript executor with evaluate method
     const bColor=await blueBox.evaluate((el) => {
        return window.getComputedStyle(el).getPropertyValue('background-color');
     })
     expect(bColor).toEqual("rgb(63, 81, 181)")
+    */
 
 })
 
